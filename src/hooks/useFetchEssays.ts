1a250c68042e36093d16b9e81afc39e8d242f88c
Joshwin Greene <joshwin.greene@gmail.com>
2023-09-17 17:48:16 -0400

    Corrected the errors on the useFetchEssays page

--- src/hooks/useFetchEssays.ts
+++ src/hooks/useFetchEssays.ts
@@ -1,24 +1,26 @@
 import { useEffect, useState } from "react";
-import { Piece } from '@/types/manual';
+import { Published_Piece_View } from '@/types/manual';
 import { useSupabaseClient } from '@supabase/auth-helpers-react'
 import { Database } from '../types/generated';
 
 export const useFetchEssays = () => {
-    const [essays, setEssays] = useState<Piece[]>([])
+    const [essays, setEssays] = useState<Published_Piece_View[]>([])
     const supabaseClient = useSupabaseClient<Database>();
     
-    const fetchEssays = async () => {
-        const { data, error } = await supabaseClient
-            .from('published_piece_view')
-            .select('*')
-            .eq('type', 'Essay')
-        if(data) 
-            console.log(data),
-            setEssays(data)
-        if(error) console.error('error -', error)
-    }
-    
-    useEffect(() => { fetchEssays() }, [supabaseClient]);
+    useEffect(() => { 
+        const fetchEssays = async () => {
+            const { data, error } = await supabaseClient
+                .from('published_piece_view')
+                .select('*')
+                .eq('type', 'Essay')
+            if(data) 
+                console.log(data),
+                setEssays(data)
+            if(error) console.error('error -', error)
+        }
+
+        fetchEssays(); 
+    }, [supabaseClient]);
 
     return { essays };
 }
