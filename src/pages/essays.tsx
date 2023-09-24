b3eb5afa06b8f40fa5361e8dc8bf7dc9db1786d7
Ashley Crouch <ashley.crouch@rocketmail.com>
2023-09-24 13:12:55 -0500

    Ashley essays page (#16)
    * initial commit for PieceCard component
    
    * PieceCard now renders without errors in index page
    
    * Adding more content to the card
    
    * Added image to card
    
    * added some stuff to the storybook, trying to get storybook to run
    
    * removing PieceCard from index and adding it to storybook
    
    * Moving pieceCard component to its own folder
    
    * initial essays page made
    
    * added the router, which is returning null
    
    * essays is kind of working (missing contributors in the fetch)
    
    * Fixes to the essay hook and added use for the contributor hook
    
    * changing contributor to be a default for now
    
    * made some layout changes to essays and code cleanup
    
    * more code cleanup
    
    * fixed name not showing up in the card with the sample contributor and useFetchEssays error so that it has a dependency for supabaseClient. Also changed the PieceCard storybook to be more consistent with other storybooks.
    
    * Corrected the errors on the useFetchEssays page
    
    ---------
    
    Co-authored-by: Joshwin Greene <joshwin.greene@gmail.com>

---
+++ src/pages/essays.tsx
@@ -0,0 +1,43 @@
+import { ReactElement } from 'react';
+import Head from 'next/head';
+import { NextPageWithLayout } from './_app';
+import { Container, Stack } from '@chakra-ui/react';
+import { PageHeader } from '@/components/PageHeader/PageHeader';
+import { useFetchEssays } from '@/hooks/useFetchEssays';
+import { PieceCard } from '@/components/PieceCard/PieceCard';
+// import { useFetchContributorForPiece } from '@/hooks/useFetchContributorForPiece'; //may use later when we add contributors in
+import { Published_Piece_View } from '../types/manual';
+import { Layout } from '@/components/layouts/Layout';
+// Add the page's header using the PageHeader component
+// Show the fetched essays in a list (use the PieceCard component for each item)
+
+const Essays: NextPageWithLayout = () => {
+    const { essays } = useFetchEssays();
+
+    return <>
+        <Head>
+            <title>Essays</title>
+        </Head>
+        <Container>
+            <PageHeader title="Essays" subtitle="A collection of atomic notes, i.e. a single idea or a single object of attention." />
+            <Stack spacing={4}>{
+                essays && essays.map((essay: Published_Piece_View, index: number) => 
+                <PieceCard key={index} piece={essay} contributor={{
+                    id: "1", 
+                    avatar_url: "", 
+                    bio: "I like coding", 
+                    username: "TheeSamSmith01", 
+                    full_name: "Sam Smith", 
+                    created_at:"", 
+                    updated_at:""
+                }}/>)
+            }</Stack>
+        </Container>
+    </>
+}
+
+Essays.getLayout = function getLayout(page: ReactElement){
+    return <Layout>{page}</Layout>
+}
+
+export default Essays;
